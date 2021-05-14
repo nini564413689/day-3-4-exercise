e11329d87b105dd7c3bdfcd2d7738895e4a4beaa
Qianni Zhang <>
2021-05-14 03:00:55 +0000

    Initial commit

---
+++ way2.py
@@ -0,0 +1,26 @@
+# 🚨 Don't change the code below 👇
+print("Welcome to Python Pizza Deliveries!")
+size = input("What size pizza do you want? S, M, or L ")
+add_pepperoni = input("Do you want pepperoni? Y or N ")
+extra_cheese = input("Do you want extra cheese? Y or N ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+total_bill = 0
+if size == "S":
+  total_bill += 15
+elif size == "M":
+  total_bill += 20
+elif size == "L":
+  total_bill += 25
+
+if add_pepperoni == "Y":
+  if size == "S":
+    total_bill += 2
+  else:
+    total_bill += 3
+
+if   extra_cheese == "Y":
+  total_bill += 1
+
+print (f'Your final bill is {total_bill}')
